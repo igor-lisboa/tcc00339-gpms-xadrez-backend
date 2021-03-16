1e6fe30e71cd62be6a542cdba4889a5c15b729ad
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-03-16 15:40:06 -0300

    Implementando tratamento de sockets

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const cors = require("cors");
 const routes = require("./src/routes");
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const jogadoresConectados = {};
+
+io.on('connection', (socket) => {
+    const { user } = socket.handshake.query;
+
+    jogadoresConectados[user] = socket.id;
+});
+
+// middleware pra registrar o socket e os jogadores conectados no request
+app.use((req, res, next) => {
+    req.io = io;
+    req.jogadoresConectados = jogadoresConectados;
+
+    next();
+});
 
 app.use(cors());
 app.use(express.json());
@@ -10,4 +28,4 @@ app.use(routes);
 
 const port = process.env.PORT || 3333;
 
-app.listen(port);
+http.listen(port);
