6a31baf1e7f61b91715629102f100b01fc61bcd8
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-03-20 14:51:48 -0300

    Retirando endpoints destinados ao uso da ia

--- ia.js
+++ ia.js
@@ -1,10 +1,14 @@
+const JogoService = require("./src/services/JogoService");
+
 const sleep = async (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const ia = async () => {
     while (true) {
-        const db = require("./src/database.json");
+        const jogosIa = JogoService.listaIa();
+
+        JogoService.recuperaLadosIa(jogoId)
 
         console.log('teste' + new Date().getTime());
         await sleep(5000);
