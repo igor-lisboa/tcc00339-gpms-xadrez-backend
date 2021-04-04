7430651d986f56db3c1cffab0b7866dfd9c2be51
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-04-04 20:47:08 -0300

    Colocando msg socket de aviso de jogadaRealizada p enviar a jogadaRealizada

--- src/controllers/JogoPecaController.js
+++ src/controllers/JogoPecaController.js
@@ -60,7 +60,7 @@ module.exports = {
 
             // se encontrar o adversario na lista de jogadores conectados dispara evento p socket do adversario
             if (destinoEvento != undefined) {
-                req.io.to(destinoEvento.socketId).emit('jogadaRealizada');
+                req.io.to(destinoEvento.socketId).emit('jogadaRealizada', jogadaRealizada);
                 if (req.verbose) {
                     console.log("Enviando mensagem de jogadaRealizada para " + destinoEvento.identificador + "...");
                 }
